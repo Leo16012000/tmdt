c4d1badd3887e318b0ab65ac5b41c0c7de8d4dfa
sanghuynh <sanghuynh20000@gmail.com>
2021-03-09 16:04:56 +0700

    OK Home__catalog

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,16 +16,99 @@ const sliderImage = [
 	{ src: "https://theme.hstatic.net/1000360516/1000609234/14/slideshow_9.png" },
 ];
 
+const catalogImage1 = [
+	{
+		src: "https://theme.hstatic.net/1000360516/1000609234/14/x1.jpg?v=428",
+		name: "GHẾ SOFA",
+	},
+	{
+		src: "https://theme.hstatic.net/1000360516/1000609234/14/x2.jpg?v=428",
+		name: "GIƯỜNG",
+	},
+	{
+		src: "https://theme.hstatic.net/1000360516/1000609234/14/x3.jpg?v=428",
+		name: "BÀN ĂN",
+	},
+	{
+		src: "https://theme.hstatic.net/1000360516/1000609234/14/x4.jpg?v=428",
+		name: "BÀN LÀM VIỆC",
+	},
+	{
+		src: "https://theme.hstatic.net/1000360516/1000609234/14/x55.jpg?v=428",
+		name: "BÀN TRÀ",
+	},
+];
+
+const catalogImage2 = [
+	{
+		src: "https://theme.hstatic.net/1000360516/1000609234/14/x6.jpg?v=428",
+		name: "BÀN TRANG ĐIỂM	",
+	},
+	{
+		src: "https://theme.hstatic.net/1000360516/1000609234/14/x7.jpg?v=428",
+		name: "TỦ TRANG TRÍ",
+	},
+	{
+		src: "https://theme.hstatic.net/1000360516/1000609234/14/x8.jpg?v=428",
+		name: "KỆ TRANG TRÍ",
+	},
+	{
+		src: "https://theme.hstatic.net/1000360516/1000609234/14/x9.jpg?v=428",
+		name: "TỦ ĐẦU GIƯỜNG",
+	},
+	{
+		src: "https://theme.hstatic.net/1000360516/1000609234/14/x100.jpg?v=428",
+		name: "GIÁ TREO QUẦN ÁO",
+	},
+];
+
+function Home__carousel() {
+	return (
+		<div className="Home__carousel">
+			<AwesomeSlider cssModule={AwsSliderStyles}>
+				{sliderImage.map((item, index) => (
+					<div data-src={item.src} key={index} />
+				))}
+			</AwesomeSlider>
+		</div>
+	);
+}
+
+function Home__catalog() {
+	return (
+		<div className="Home__catalog">
+			<div className="content">
+				<h1>DANH MỤC NỔI BẬT</h1>
+				<p>
+					Danh mục sản phẩm bán tốt nhất trong tháng và được lựa chọn nhiều
+					nhất.
+				</p>
+			</div>
+			<div className="banner">
+				{catalogImage1.map((item, index) => (
+					<div className="banner__item" key={index}>
+						<img src={item.src} />
+						<h4>{item.name}</h4>
+					</div>
+				))}
+			</div>
+			<div className="banner">
+				{catalogImage2.map((item, index) => (
+					<div className="banner__item" key={index}>
+						<img src={item.src} />
+						<h4>{item.name}</h4>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
+
 function Home(props) {
 	return (
 		<div className="Home">
-			<div className="Home__carousel">
-				<AwesomeSlider cssModule={AwsSliderStyles}>
-					{sliderImage.map((item) => (
-						<div data-src={item.src} />
-					))}
-				</AwesomeSlider>
-			</div>
+			<Home__carousel />
+			<Home__catalog />
 		</div>
 	);
 }
