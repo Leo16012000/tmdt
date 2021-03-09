35b0584edaffffed5f3003106b5234b1ad36ff5f
sanghuynh <sanghuynh20000@gmail.com>
2021-03-09 15:05:41 +0700

    OK carousel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,6 @@ function App() {
 		<div className="App">
 			<Router>
 				<TopNavigation />
-				Loading
 				<Suspense fallback={<h1>....</h1>}>
 					<Switch>
 						<Route exact path="/">
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,33 @@
 import React from "react";
-import PropTypes from "prop-types";
+import AwesomeSlider from "react-awesome-slider";
+import AwsSliderStyles from "react-awesome-slider/src/core/styles.scss";
 
-Home.propTypes = {};
+import "../styles/Home.css";
+
+const sliderImage = [
+	{ src: "https://theme.hstatic.net/1000360516/1000609234/14/slideshow_1.png" },
+	{ src: "https://theme.hstatic.net/1000360516/1000609234/14/slideshow_2.png" },
+	{ src: "https://theme.hstatic.net/1000360516/1000609234/14/slideshow_3.png" },
+	{ src: "https://theme.hstatic.net/1000360516/1000609234/14/slideshow_4.png" },
+	{ src: "https://theme.hstatic.net/1000360516/1000609234/14/slideshow_5.png" },
+	{ src: "https://theme.hstatic.net/1000360516/1000609234/14/slideshow_6.png" },
+	{ src: "https://theme.hstatic.net/1000360516/1000609234/14/slideshow_7.png" },
+	{ src: "https://theme.hstatic.net/1000360516/1000609234/14/slideshow_8.png" },
+	{ src: "https://theme.hstatic.net/1000360516/1000609234/14/slideshow_9.png" },
+];
 
 function Home(props) {
-  return <div>Home Page</div>;
+	return (
+		<div className="Home">
+			<div className="Home__carousel">
+				<AwesomeSlider cssModule={AwsSliderStyles}>
+					{sliderImage.map((item) => (
+						<div data-src={item.src} />
+					))}
+				</AwesomeSlider>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
