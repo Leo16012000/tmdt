3451bfdeae634002e7f27073f306c99cbab55f66
Leo16012000 <67458956+Leo16012000@users.noreply.github.com>
2021-05-09 15:42:48 +0700

    create order from cart finsih

---
+++ client/src/pages/UpdateOrder.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import sendMessage from "../account/sendMessage";
+import { Button } from "@material-ui/core";
+import { useSelector } from "react-redux";
+
+function UpdateOrder(props) {
+  const axios = require("axios");
+  const listCart = useSelector((state) => state.listCart);
+  // change name of object
+  listCart.map((item) => {
+    item.code = item.id.toString();
+    item.price = item.unitCost;
+  });
+  // console.log(listCart); done
+
+  //
+  const items = [...listCart];
+  // console.log(items); done
+  const bodyParameter = {
+    payment_type_id: 2,
+    note: "Tintest 123",
+    required_note: "KHONGCHOXEMHANG",
+    return_phone: "0353323643",
+    return_address: "39 NTT",
+    return_district_id: null,
+    return_ward_code: "",
+    client_order_code: "",
+    to_name: "TinTest124", //
+    to_phone: "0987654321", //
+    to_address: "72 Thành Thái, Phường 14, Quận 10, Hồ Chí Minh, Vietnam", //
+    to_ward_code: "20308", //
+    to_district_id: 1444, //
+    cod_amount: 200000, //
+    content: "Theo New York Times",
+    weight: 200,
+    length: 1,
+    width: 19,
+    height: 10,
+    pick_station_id: 1444,
+    deliver_station_id: null,
+    insurance_value: 10000000,
+    service_id: 0,
+    service_type_id: 2,
+    order_value: 130000,
+    coupon: null,
+    pick_shift: [2],
+    items,
+  };
+  const config = {
+    headers: {
+      token: "69ace0ab-ac82-11eb-8be2-c21e19fc6803",
+      ShopId: 79749,
+    },
+  };
+  function orderCreate() {
+    axios
+      .post(
+        "https://dev-online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/create",
+        bodyParameter,
+        config
+      )
+      .then((res) => {
+        console.log(res.data, "run into here");
+      })
+      .catch((err) => console.log(err));
+    return <div>Run Update Order</div>;
+  }
+  orderCreate();
+  return <div></div>;
+}
+
+export default UpdateOrder;
