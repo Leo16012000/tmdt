36a428f77ee48bc8298a4065d48c146b0e27fd29
*NLm2wyCxRX <i-2021-dangnhatquan@fois-vn.com>
2021-05-10 10:39:47 +0700

    push for sang

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,10 +1,12 @@
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect, Suspense } from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import 'antd/dist/antd.css';
 import '../styles/Admin.css'
 import { Layout, Menu, Breadcrumb } from 'antd';
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 import Axios from "axios";
 import { Table, Tag, Space } from 'antd';
+import Collections from "./Collections";
 const { Column, ColumnGroup } = Table;
 
 
@@ -37,7 +39,12 @@ function Admin() {
                         <Menu.Item key="1">Danh sách khách hàng</Menu.Item>
                     </SubMenu>
                     <SubMenu key="sub2" icon={<LaptopOutlined />} title="Thông tin sản phẩm">
-                        <Menu.Item key="5">Danh sách thông tin sản phẩm</Menu.Item>
+                        <Menu.Item key="5">
+                            <Link to="/admin/collections">
+                                Danh sách thông tin sản phẩm
+                            </Link>
+                            
+                        </Menu.Item>
                         <Menu.Item key="6">Thêm sản phẩm mới</Menu.Item>
                     </SubMenu>
                     <SubMenu key="sub3" icon={<NotificationOutlined />} title="Đơn hàng">
@@ -60,12 +67,49 @@ function Admin() {
                         minHeight: 280,
                     }}
                     >
-                    <Table dataSource={item}>
-                            <Column title="ID" dataIndex="id" key="id" />
-                            <Column title="Name" dataIndex="name" key="name" />
-                            <Column title="Price" dataIndex="price" key="price" />
-                            <Column title="Price" dataIndex="price" key="price" />
-                            <Column 
+
+                    Danh sách sản phẩm
+
+                    <Router>
+                        <Suspense fallback={<h1>....</h1>}>
+                            <Switch>
+                                <Route exact path="admin/collections" >
+                                    <CollectionData />
+                                </Route>
+
+                                <Route exact path="admin/collections" >
+                                    <UserData />
+                                </Route>
+                            </Switch>
+                        </Suspense>
+                    </Router>
+                    
+                    
+                    </Content>
+                </Layout>
+                </Layout>
+            </Layout>
+        </div>
+    )
+}
+
+
+function CollectionData(){
+    const [item, setItem] = useState([]);
+
+    useEffect(() => {
+        Axios.get(`http://localhost:3001/collections`).then((response) => {
+        setItem(response.data);
+        });
+    }, []);
+
+    return (
+        <Table dataSource={item}>
+                            <Column title="ID" dataIndex="ID" key="ID" />
+                            <Column title="Name" dataIndex="Fullname" key="Fullname" />
+                            <Column title="Price" dataIndex="Price" key="Price" />
+                            <Column title="State" dataIndex="State" key="State" />
+                            {/* <Column 
                             title="Tags"
                             dataIndex="tags"
                             key="tags"
@@ -88,14 +132,53 @@ function Admin() {
                                 <a>Delete</a>
                                 </Space>
                             )}
-                            />
+                            /> */}
                         </Table>
-                    </Content>
-                </Layout>
-                </Layout>
-            </Layout>
-        </div>
     )
 }
+
+function UserData(){
+    const [item, setItem] = useState([]);
+
+    useEffect(() => {
+        Axios.get(`http://localhost:3001/users`).then((response) => {
+        setItem(response.data);
+        });
+    }, []);
+
+    return (
+        <Table dataSource={item}>
+            <Column title="ID" dataIndex="ID" key="ID" />
+            <Column title="Name" dataIndex="Fullname" key="Fullname" />
+            <Column title="Price" dataIndex="Price" key="Price" />
+            <Column title="State" dataIndex="State" key="State" />
+                            {/* <Column 
+                            title="Tags"
+                            dataIndex="tags"
+                            key="tags"
+                            render={tags => (
+                                <>
+                                {tags.map(tag => (
+                                    <Tag color="blue" key={tag}>
+                                    {tag}
+                                    </Tag>
+                                ))}
+                                </>
+                            )}
+                            />
+                            <Column
+                            title="Action"
+                            key="action"
+                            render={(text, record) => (
+                                <Space size="middle">
+                                <a>Invite {record.lastName}</a>
+                                <a>Delete</a>
+                                </Space>
+                            )}
+                            /> */}
+        </Table>
+    )
+}
+
 
 export default Admin
